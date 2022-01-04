5de82086baba4aa6efe0b1e6e0100d1ca93414cc
Ethan Brown <ewb@ewb.io>
2022-01-05 00:17:34 +0100

    fourth

--- 2022/4/src/main.rs
+++ 2022/4/src/main.rs
@@ -5,24 +5,21 @@ use rand_pcg::Pcg64;
 const SEED: u64 = 12345;
 const WINDOW_X: usize = 1000;
 const WINDOW_Y: usize = 1000;
-const SPIN_WIDTH_X: usize = 20;
-const SPIN_WIDTH_Y: usize = 20;
-const N_HIGHLIGHT_STEPS: usize = 50;
+const N_VISIBLE_LINES: usize = 10000;
 
 #[derive(Copy, Clone)]
 struct Line {
     start: Point2,
     end: Point2,
-    highlight_start: Point2,
-    highlight_end: Point2,
-    step: usize,
-    n_highlight_steps: usize
+    is_visible: bool
 }
 
 struct Model {
     _window: window::Id,
     lines: Vec<Line>,
-    rng: rand_pcg::Pcg64
+    rng: rand_pcg::Pcg64,
+    whx: f32,
+    why: f32
 }
 
 fn model(app: &App) -> Model {
@@ -32,91 +29,94 @@ fn model(app: &App) -> Model {
         .view(view)
         .build()
         .unwrap();
-    let rng = Pcg64::seed_from_u64(SEED);
-    let window = app.main_window();
-    let wh = window.rect();
-    let n_x: usize = wh.w() as usize / SPIN_WIDTH_X as usize;
-    let n_y: usize = wh.h() as usize / SPIN_WIDTH_Y as usize;
-    let w_x: f32 = (n_x * SPIN_WIDTH_X) as f32;
-    let w_y: f32 = (n_y * SPIN_WIDTH_Y) as f32;
+    let mut rng = Pcg64::seed_from_u64(SEED);
+    let whx = WINDOW_X as f32;
+    let why = WINDOW_Y as f32;
 
     let mut lines: Vec<Line> = Vec::new();
-    for i in 0..n_x {
-        for j in 0..n_y {
-            let x: f32 = (i * SPIN_WIDTH_X) as f32 / w_x;
-            let y: f32 = (j * SPIN_WIDTH_Y) as f32 / w_y;
-            let xp: f32 = 2.0*x*x + y;
-            let yp: f32 = x*x;
-            let vx: f32 = xp - x;
-            let vy: f32 = yp - y;
-            let theta = (vy/vx).atan();
-            let r = 100.0 * (vx*vx + vy*vy).sqrt() / 2.0;
-            let px: f32 = (i * SPIN_WIDTH_X) as f32 - 0.5 * w_x;
-            let py: f32 = (j * SPIN_WIDTH_Y) as f32 - 0.5 * w_y;
-            let start_x: f32 = (px - 0.5 * r * theta.cos()).min(0.5*w_x).max(-0.5*w_x);
-            let start_y: f32 = (py - 0.5 * r * theta.sin()).min(0.5*w_y).max(-0.5*w_y);
-            let end_x: f32 = (px + 0.5 * r * theta.cos()).min(0.5*w_x).max(-0.5*w_x);
-            let end_y: f32 = (py + 0.5 * r * theta.sin()).min(0.5*w_y).max(-0.5*w_y);
-
-            let dx = end_x - start_x;
-            let dy = end_y - start_y;
-            lines.push(Line{
-                start: Point2::new(start_x, start_y),
-                end: Point2::new(end_x, end_y),
-                highlight_start: Point2::new(start_x, start_y),
-                highlight_end: Point2::new(start_x + dx, start_y + dy),
-                step: 0,
-                n_highlight_steps: N_HIGHLIGHT_STEPS
-            });
-        }
+    while lines.len() < N_VISIBLE_LINES {
+        let start_x = rng.gen_range(-whx/2.0..whx/2.0);
+        let start_y = rng.gen_range(-why/2.0..why/2.0);
+        let line: Line = Line{
+            start: Point2::new(start_x, start_y),
+            end: Point2::new(start_x, start_y),
+            is_visible: true
+        };
+        lines.push(line);
     }
 
     Model {
         _window,
         lines,
         rng,
+        whx,
+        why
     }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
+    // remove lines
+    _model.lines.retain(|l| l.is_visible);
+
+    // add lines
+    //while _model.lines.len() < N_VISIBLE_LINES as usize {
+    for _ in 0..100 {
+        let start_x = _model.rng.gen_range(-_model.whx/2.0.._model.whx/2.0);
+        let start_y = _model.rng.gen_range(-_model.why/2.0.._model.why/2.0);
+        let line: Line = Line{
+            start: Point2::new(start_x, start_y),
+            end: Point2::new(start_x, start_y),
+            is_visible: true
+        };
+        _model.lines.push(line);
+    }
+
+    // evolve lines
     for line in _model.lines.iter_mut() {
-        let sx = line.start.to_array()[0];
-        let sy = line.start.to_array()[1];
-        let ex = line.end.to_array()[0];
-        let ey = line.end.to_array()[1];
-        let hsx = line.highlight_start.to_array()[0];
-        let hsy = line.highlight_start.to_array()[1];
-        let hex = line.highlight_end.to_array()[0];
-        let hey = line.highlight_end.to_array()[1];
+        let start_x = line.start.to_array()[0];
+        let start_y = line.start.to_array()[1];
+        let end_x = line.end.to_array()[0];
+        let end_y = line.end.to_array()[1];
 
-        let dy = ey - sy;
-        let dx = ex - sx;
+        let px: f32 = (start_x + end_x) / 2.0;
+        let py: f32 = (start_y + end_y) / 2.0;
 
-        line.step += 1;
-        if line.step >= line.n_highlight_steps {
-            let hexp = sx + dx;
-            let heyp = sy + dy;
-            line.highlight_start = line.start;
-            line.highlight_end = Point2::new(hexp, heyp);
-            line.step = 0;
+        let x: f32 = 0.5 + px / _model.whx;
+        let y: f32 = 0.5 + py / _model.why;
+        let xp: f32 = 2.0*x*x + y;
+        let yp: f32 = x*x;
+        let vx: f32 = xp - x;
+        let vy: f32 = yp - y;
+        let theta = (vy/vx).atan();
+        let r = 100.0 * (vx*vx + vy*vy).sqrt() / 2.0;
+
+        let xa = (px + vx) - 0.5 * r * theta.cos();
+        let ya = (py + vy) - 0.5 * r * theta.sin();
+        let xb = (px + vx) + 0.5 * r * theta.cos();
+        let yb = (py + vy) + 0.5 * r * theta.sin();
+
+        let new_start_x = if vx < 0.0 { xb } else { xa };
+        let new_end_x = if vx < 0.0 { xa } else { xb };
+        let new_start_y = if vx < 0.0 { yb } else { ya };
+        let new_end_y = if vx < 0.0 { ya } else { yb };
+
+        line.start = Point2::new(new_start_x, new_start_y);
+        line.end = Point2::new(new_end_x, new_end_y);
+
+        // enforce death
+        if new_end_x.abs() < _model.whx/2.0 || new_end_y.abs() < _model.why/2.0 {
+            line.is_visible = true;
         } else {
-            let hsxp = hsx + dx * (1.0 / line.n_highlight_steps as f32);
-            let hsyp = hsy + dy * (1.0 / line.n_highlight_steps as f32);
-            let hexp = hex + dx * (1.0 / line.n_highlight_steps as f32);
-            let heyp = hey + dy * (1.0 / line.n_highlight_steps as f32);
-            line.highlight_start = Point2::new(hsxp, hsyp);
-            line.highlight_end = Point2::new(hexp, heyp);
+            line.is_visible = false;
         }
     }
 }
 
 fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
-    //draw.background().color(BLACK);
-
+    draw.background().color(BLACK);
     for line in _model.lines.iter() {
-        // draw.line().color(WHITE).weight(5.0).points(line.start, line.end);
-        draw.line().color(PINK).weight(1.0).points(line.highlight_start, line.highlight_end);
+        draw.line().color(PINK).weight(1.0).points(line.start, line.end);
     }
     draw.to_frame(app, &frame).unwrap();
 
