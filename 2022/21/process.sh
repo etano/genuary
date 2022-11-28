1e1bb6e7179465f23baa63bde203c03a7ca00163
Ethan Brown <ewb@ewb.io>
2022-11-28 09:59:57 -0800

    updates

--- 2022/21/process.sh
+++ 2022/21/process.sh
@@ -2,5 +2,6 @@
 
 # ffmpeg -y -i gen/%03d.png -vf fps=60,scale=flags=lanczos,palettegen gen/palette.png
 # ffmpeg -framerate 60 -i gen/%03d.png -i gen/palette.png -filter_complex "fps=60,scale=600:600:flags=lanczos[x];[x][1:v]paletteuse" -y -c:v libx264 -crf 18 -an -preset veryslow -flags +cgop -profile:v high -level 4.0 -b:v "2048k" -movflags +faststart -pix_fmt yuv420p gen/output.mp4
-ffmpeg -framerate 60 -i gen/%03d.png -filter_complex "fps=60,scale=600:600" -y -c:v libx264 -crf 18 -an -preset veryslow -flags +cgop -profile:v high -level 4.0 -b:v "2048k" -movflags +faststart -pix_fmt yuv420p gen/output.mp4
+#ffmpeg -framerate 60 -i gen/%03d.png -filter_complex "fps=60,scale=600:600" -y -c:v libx264 -crf 18 -an -preset veryslow -flags +cgop -profile:v high -level 4.0 -b:v "2048k" -movflags +faststart -pix_fmt yuv420p gen/output.mp4
+ffmpeg -framerate 60 -i gen/%03d.png -filter_complex "fps=60,scale=600:600" -y -c:v libx264 -crf 8 -an -preset veryslow -flags +cgop -profile:v high -level 4.0 -b:v "2048k" -movflags +faststart -pix_fmt yuv420p gen/output.mp4
 
